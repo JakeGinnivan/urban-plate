bb165f7400de4e013e4e2c61285369c70c16a8ab
Jake Ginnivan <jake@ginnivan.net>
2016-06-08 15:13:22 +0800

    Broke out a recipe-list component

---
+++ app/pages/recipes/components/recipe-list.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Pager, PageItem } from 'react-bootstrap'
+import { Link } from 'react-router'
+import styles from './recipe-list.module.scss'
+
+const RecipeList = ({ recipes }) => (
+  <div>
+    {recipes.length === 0 && <div>No recipes</div>}
+    {recipes.map(r => (
+      <Link to={`/recipes/${r.id}`}>{r.name}</Link>
+    ))}
+    <Pager>
+      <PageItem previous href='#'>&larr; Previous Page</PageItem>
+      <PageItem next href='#'>Next Page &rarr; </PageItem>
+    </Pager>
+  </div>
+)
+
+RecipeList.propTypes = {
+  recipes: React.PropTypes.array
+}
+
+export default RecipeList
--- app/pages/recipes/index.js
+++ app/pages/recipes/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { asyncConnect } from 'redux-connect'
-import { Pager, PageItem } from 'react-bootstrap'
 import { loadRecipes } from '../../app.redux'
-import { Link } from 'react-router'
+import RecipeList from './components/recipe-list'
 
 @asyncConnect([{
   promise: (props) => Promise.all([
@@ -16,14 +15,7 @@ import { Link } from 'react-router'
 export default class RecipeIndex extends React.Component {
   render() {
     return (
-      <div>
-        {this.props.recipes.length === 0 && <div>No recipes</div>}
-        {this.props.recipes.map(r => <Link to={`/recipes/${r.id}`}>{r.name}</Link>)}
-        <Pager>
-          <PageItem previous href='#'>&larr; Previous Page</PageItem>
-          <PageItem next href='#'>Next Page &rarr; </PageItem>
-        </Pager>
-      </div>
+      <RecipeList recipes={this.props.recipes} />
     )
   }
 }
