05ba8a2c481bcd4220ac650bab20d6858f695097
Martin Leech <leech.martin@gmail.com>
2016-09-23 10:21:55 +0800

    Fix error on ingredients page

--- app/pages/ingredients/index.js
+++ app/pages/ingredients/index.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 import { asyncConnect } from 'redux-connect'
 import { loadIngredients } from '../../ingredients.redux'
-import styles from './styles.module.scss'
+import * as styles from './styles.module.scss'
 import { Link } from 'react-router'
 import { Button } from 'react-bootstrap'
 
